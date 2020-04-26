a2eb294fc3e4b245a199f2488eee78ae1d32f29c
Charles Sprayberry <me@cspray.io>
2020-04-26 14:02:39 -0400

    Convert PromsieCombinator to yape@2.0

--- src/PromiseCombinator.php
+++ src/PromiseCombinator.php
@@ -4,115 +4,53 @@ namespace Cspray\Labrador\AsyncEvent;
 
 use Amp\Promise;
 use Cspray\Yape\Enum;
+use Cspray\Yape\EnumTrait;
 
-use function Amp\Promise\all;
-use function Amp\Promise\any;
-use function Amp\Promise\first;
-use function Amp\Promise\some;
-
-/**
- * An Enum that represents how an EventEmitter will combine the Promises that event listeners generate.
- *
- * @package Cspray\Labrador\AsyncEvent
- * @license See LICENSE in source root
- */
 final class PromiseCombinator implements Enum {
 
-    private static $container = [];
+    use EnumTrait;
 
-    private $enumConstName;
-    private $value;
+    /**
+     * @var callable
+     */
     private $combinator;
 
-    private function __construct(string $enumConstName, string $value, callable $combinator) {
-        $this->enumConstName = $enumConstName;
-        $this->value = $value;
+    private function __construct(string $value, callable $combinator) {
+        $this->setEnumValue($value);
         $this->combinator = $combinator;
     }
 
-    protected static function getSingleton($value, ...$additionalConstructorArgs) {
-        if (!isset(self::$container[$value])) {
-            self::$container[$value] = new self(...array_merge([$value], $additionalConstructorArgs));
-        }
-
-        return self::$container[$value];
-    }
-
-    /**
-     * A PromiseCombinator that uses Amp\Promise\all to combine the Promises.
-     *
-     * @return PromiseCombinator
-     */
-    public static function All() : PromiseCombinator {
-        return self::getSingleton('All', '\\Amp\\Promise\\all', function(Promise ...$promises) {
-            return all($promises);
+    public static function All() : self {
+        return self::getSingleton('All', function(Promise ...$promises) {
+            return Promise\all($promises);
         });
     }
 
-    /**
-     * A PromiseCombinator that uses Amp\Promise\any to combine the Promises.
-     *
-     * @return PromiseCombinator
-     */
-    public static function Any() : PromiseCombinator {
-        return self::getSingleton('Any', '\\Amp\\Promise\\any', function(Promise ...$promises) {
-            return any($promises);
+    public static function Any() : self {
+        return self::getSingleton('Any', function(Promise ...$promises) {
+            return Promise\any($promises);
         });
     }
 
-    /**
-     * A PromiseCombinator that uses Amp\Promise\first to combine the Promises.
-     *
-     * @return PromiseCombinator
-     */
-    public static function First() : PromiseCombinator {
-        return self::getSingleton('First', '\\Amp\\Promise\\first', function(Promise ...$promises) {
-            return first($promises);
+    public static function First() : self {
+        return self::getSingleton('First', function(Promise ...$promises) {
+            return Promise\first($promises);
         });
     }
 
-    /**
-     * A PromiseCombinator that uses Amp\Promise\some to combine the Promises.
-     *
-     * @return PromiseCombinator
-     */
-    public static function Some() : PromiseCombinator {
-        return self::getSingleton('Some', '\\Amp\\Promise\\some', function(Promise ...$promises) {
-            return some($promises);
+    public static function Some() : self {
+        return self::getSingleton('Some', function(Promise ...$promises) {
+            return Promise\some($promises);
         });
     }
 
-    /**
-     * Returns the fully qualified method name that this PromiseCombinator represents.
-     *
-     * @return string
-     */
-    public function getValue() : string {
-        return $this->value;
+    // It is imperative that if you add a new value post code generation you add the method name here!
+    static protected function getAllowedValues() : array {
+        return ['All', 'Any', 'First', 'Some', ];
     }
 
-    /**
-     * Combines the Promises using the Amp Promise function this enum represents.
-     *
-     * @param Promise ...$promises
-     * @return Promise
-     */
     public function combine(Promise ...$promises) : Promise {
         return ($this->combinator)(...$promises);
     }
 
-    /**
-     * @param PromiseCombinator $promiseCombinator
-     * @return bool
-     */
-    public function equals(PromiseCombinator $promiseCombinator) : bool {
-        return $this === $promiseCombinator;
-    }
-
-    /**
-     * @return string
-     */
-    public function toString() : string {
-        return get_class($this) . '@' . $this->enumConstName;
-    }
 }
