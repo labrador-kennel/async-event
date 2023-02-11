7be8ba52d2321d1c39bbe910f2d4d78a9d859484
Charles Sprayberry <me@cspray.io>
2023-02-11 15:35:32 -0500

    Update Annotated Container, add ThirdPartyInitializer

---
+++ test/Unit/AsyncEventInitializerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Labrador\AsyncEvent\Test\Unit;
+
+use Labrador\AsyncEvent\DependencyInjection\AsyncEventInitializer;
+use Labrador\AsyncEvent\DependencyInjection\AutowireObserver;
+use PHPUnit\Framework\TestCase;
+
+final class AsyncEventInitializerTest extends TestCase {
+
+    public function testGetPackageName() : void {
+        $actual = (new AsyncEventInitializer())->getPackageName();
+
+        self::assertSame('labrador-kennel/async-event', $actual);
+    }
+
+    public function testGetScanDirectories() : void {
+        $actual = (new AsyncEventInitializer())->getRelativeScanDirectories();
+
+        self::assertSame(['src'], $actual);
+    }
+
+    public function testGetObservers() : void {
+        $actual = (new AsyncEventInitializer())->getObserverClasses();
+
+        self::assertSame([AutowireObserver::class], $actual);
+    }
+
+    public function testGetDefinitionProvider() : void {
+        $actual = (new AsyncEventInitializer())->getDefinitionProviderClass();
+
+        self::assertNull($actual);
+    }
+
+
+}
